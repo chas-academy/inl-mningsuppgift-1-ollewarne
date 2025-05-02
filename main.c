cf212f5edb928abd7a68ba2a6dffd9c3cc30a244
ollewarne <55360932+Lousio-n@users.noreply.github.com>
2025-05-02 23:48:34 +0200

    solution

--- main.c
+++ main.c
@@ -8,6 +8,49 @@ int main()
 
    
     // Your code goes here
+    int dice_rolls[100];
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
+    int sum = 0;
+    float average = 0;
+
+    for (int i = 0; i < 100; i++){
+        int dice_roll = (rand() % 6) + 1;
+        dice_rolls[i] = dice_roll;
+    }
+
+    for (int i = 0; i < 100; i++){
+        if (dice_rolls[i] == 1){
+            one++;
+        } else if (dice_rolls[i] == 2){
+            two++;
+        } else if (dice_rolls[i] == 3){
+            three++;
+        } else if (dice_rolls[i] == 4){
+            four++;
+        } else if (dice_rolls[i] == 5){
+            five++;
+        } else if (dice_rolls[i] == 6){
+            six++;
+        }
+        sum += dice_rolls[i];
+    }
+
+    average = sum / 100.0;
+
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", three);
+    printf("%d\n", four);
+    printf("%d\n", five);
+    printf("%d\n", six);
+    printf("%d\n", sum);
+    printf("%.1f\n", average);
+
 
     return 0;
 }
